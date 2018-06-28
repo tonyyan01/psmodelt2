b51c84498001e60453c45b413abb5e4697941a35
Dec, Marek <marek.dec@sap.com>
2018-06-28 09:58:37 +0200

    add custom storefront with webroot as "/"

---
+++ customsite/customsitecockpits/gensrc/org/customsite/cockpits/constants/GeneratedCustomsiteCockpitsConstants.java
@@ -0,0 +1,31 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 30, 2018 9:18:56 AM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package org.customsite.cockpits.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCustomsiteCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "customsitecockpits";
+	
+	protected GeneratedCustomsiteCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ customsite/customsitefulfilmentprocess/gensrc/org/customsite/fulfilmentprocess/constants/GeneratedCustomsiteFulfilmentProcessConstants.java
@@ -0,0 +1,40 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 30, 2018 9:18:56 AM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package org.customsite.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCustomsiteFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "customsitefulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedCustomsiteFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
